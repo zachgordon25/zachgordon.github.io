442bc9b8e981ac4179cc64dd0ac17b6fd736e3cc
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-07-16 16:16:41 -0400

    added some convinience features

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,7 @@
 $(() => {
   $('form').on('click', '#submit', event => {
     event.preventDefault();
+    $('#forecast').empty();
     let $zip = $('#zip-code');
     let zipCode = $zip.val();
     let endpoint = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&APPID=27191dbc29ccfde300356183b803d41f`;
@@ -8,6 +9,7 @@ $(() => {
       // console.log(data);
       let weatherConditions = data.weather[0];
       let tempK = data.main.temp;
+      // $zip.hid();
 
       // convert Kelvin to Farenheit
       const tempConversion = () => {
@@ -21,12 +23,16 @@ $(() => {
         console.log(weatherConditions);
 
         const $city = $('<h1>').text(weatherConditions.name);
-        $('body').append($city);
-        const $temp = $('<h2>').text(weatherConditions.temp);
-        $('body').append($temp);
-        const $conditions = $('<h3>').text(weatherConditions.description);
-        $('body').append($conditions);
-        console.log($city);
+        $('#forecast').append($city);
+        const $temp = $('<h2>').text(
+          `Current temp: ${weatherConditions.temp}°F`
+        );
+        $('#forecast').append($temp);
+        const $conditions = $('<h3>').text(
+          `Current condition: ${weatherConditions.description}`
+        );
+        $('#forecast').append($conditions);
+        // console.log($city);
       };
       tempConversion();
     });
