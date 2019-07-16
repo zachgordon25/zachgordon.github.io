845089ee3706b04382013f6228c89810846f6c19
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-07-16 18:07:23 -0400

    current weather

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,6 @@
 $(() => {
-  let date = new Date().toLocaleString('en-US', {
-    timeZone: 'America/New_York'
-  });
-  console.log(date);
+  // let date = new Date();
+  // console.log(date);
 
   $('form').on('click', '#submit', event => {
     event.preventDefault();
@@ -10,23 +8,24 @@ $(() => {
     let $zip = $('#zip-code');
     let zipCode = $zip.val();
     let endpoint = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&APPID=27191dbc29ccfde300356183b803d41f`;
+    // let endpoint = `http://api.openweathermap.org/data/2.5/forecast?zip=06854&APPID=27191dbc29ccfde300356183b803d41f`;
+
     $.ajax({ url: endpoint }).then(data => {
-      console.log(data);
+      console.log(data.length);
       let weatherConditions = data.weather[0];
       let tempK = data.main.temp;
       // $zip.hid();
-
       // convert Kelvin to Farenheit
       const tempConversion = () => {
         let tempF = Math.round(((tempK - 273.15) * 9) / 5 + 32);
-
         // mold object the way I want
         weatherConditions.temp = tempF;
         weatherConditions.name = data.name;
         delete weatherConditions.icon;
         delete weatherConditions.id;
         console.log(weatherConditions);
-
+        // append information to body
+        // $('#forecast').append(date);
         const $city = $('<h1>').text(weatherConditions.name);
         $('#forecast').append($city);
         const $temp = $('<h2>').text(
