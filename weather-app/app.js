caa6a9c0861b96ee0137e241fd2a01ebcdb4d751
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-07-16 12:31:28 -0400

    got api to work

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,10 @@
 $(() => {
   let zipCode = '94040';
   let countryCode = 'us';
-  let endpoint = `api.openweathermap.org/data/2.5/forecast/hourly?zip=${zipCode},${countryCode}&APPID=27191dbc29ccfde300356183b803d41f`;
-  $.ajax({ url: endpoint });
-  console.log(endpoint);
+  let endpoint = `http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=27191dbc29ccfde300356183b803d41f`;
+  $.ajax({ url: endpoint }).then(data => {
+    console.log(data);
+  });
   // const handleData = () => {
   //   console.log(endpoint);
   // };
