6d345a7b37f13362cadc8aca385488aec2cc8705
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-07-17 09:35:35 -0400

    linked giphy

--- weather-app/app.js
+++ weather-app/app.js
@@ -8,22 +8,27 @@ $(() => {
     let $zip = $('#zip-code');
     let zipCode = $zip.val();
     let endpoint = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&APPID=27191dbc29ccfde300356183b803d41f`;
-    // let endpoint = `http://api.openweathermap.org/data/2.5/forecast?zip=06854&APPID=27191dbc29ccfde300356183b803d41f`;
 
     $.ajax({ url: endpoint }).then(data => {
-      console.log(data.length);
       let weatherConditions = data.weather[0];
       let tempK = data.main.temp;
-      // $zip.hid();
+      let keyWord = data.weather[0].main;
+      console.log(keyWord);
+
+      // Using main weather term to search for a gif
+      let gifEndpoint = `https://api.giphy.com/v1/gifs/translate?api_key=lutoU47bgWimQRM7XenOl702O4MDlPGG&s=${keyWord}&weirdness:10`;
+      $.ajax({ url: gifEndpoint }).then(console.log(gifEndpoint));
       // convert Kelvin to Farenheit
       const tempConversion = () => {
         let tempF = Math.round(((tempK - 273.15) * 9) / 5 + 32);
+
         // mold object the way I want
         weatherConditions.temp = tempF;
         weatherConditions.name = data.name;
         delete weatherConditions.icon;
         delete weatherConditions.id;
         console.log(weatherConditions);
+
         // append information to body
         // $('#forecast').append(date);
         const $city = $('<h1>').text(weatherConditions.name);
