07f696ffb8824d532de381c0806c7343e625717a
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-07-18 12:13:47 -0400

    changed font size

--- weather-app/app.js
+++ weather-app/app.js
@@ -8,12 +8,12 @@ $(() => {
     $('#gif').empty();
     let $zip = $('#zip-code');
     let zipCode = $zip.val();
-    let endpoint = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&APPID=27191dbc29ccfde300356183b803d41f`;
+    let weatherEndpoint = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&APPID=27191dbc29ccfde300356183b803d41f`;
 
-    $.ajax({ url: endpoint }).then(data => {
+    $.ajax({ url: weatherEndpoint }).then(data => {
       let weatherConditions = data.weather[0];
       let tempK = data.main.temp;
-      console.log(endpoint);
+      console.log(`%c ${weatherEndpoint}`, 'color: green');
 
       // GIF searchword
       let keyword = data.weather[0].main;
@@ -22,15 +22,15 @@ $(() => {
       // Using main weather term to search for a gif
       let gifEndpoint = `https://api.giphy.com/v1/gifs/translate?api_key=lutoU47bgWimQRM7XenOl702O4MDlPGG&s=${keyword}`;
       // let gifEndpoint = `https://api.giphy.com/v1/gifs/search?api_key=lutoU47bgWimQRM7XenOl702O4MDlPGG&q=${keyword}&limit=25&offset=0&rating=R&lang=en`;
-      console.log(gifEndpoint);
+      console.log(`%c ${gifEndpoint}`, 'color: yellow');
       $.ajax({ url: gifEndpoint }).then(gifData => {
         // let gifImg = gifData.data[0].images.fixed_height_downsampled.url;
 
         // gif link location in JSON file
         let gifImg = gifData.data.images.fixed_height_downsampled.url;
-        console.log(gifImg);
+        console.log(`%c ${gifImg}`, 'color: blue');
         // console.log(gifData);
-        let $gifDiv = $(`<img src=${gifImg}>`);
+        let $gifDiv = $(`<img src=${gifImg}>`).attr('id', 'gif');
         let $gifTag = $(
           '<img id="badge" src="img/Poweredby_100px-White_VertLogo.png" />'
         );
@@ -55,9 +55,9 @@ $(() => {
 
         // append information to body
         // $('#forecast').append(date);
-        const $city = $('<h1>').text(weatherConditions.name);
+        const $city = $('<h2>').text(weatherConditions.name);
         $('#forecast').append($city);
-        const $temp = $('<h2>').text(
+        const $temp = $('<h3>').text(
           `Current temp: ${weatherConditions.temp}°F`
         );
         $('#forecast').append($temp);
