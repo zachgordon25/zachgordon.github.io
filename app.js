6a54362c3cf9aff10d4c4c481d2b246fd8d6e631
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-09-15 19:44:24 -0400

    made modal

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 $(() => {
   window.jQuery ? console.log('linked') : console.log('not linked');
+  // Made variables for buttons and modal
+  const $contactBtn = $('#contact-button');
+  const $close = $('#close-modal');
+  const $modal = $('#contact-modal');
+
+  // Modal shows
+  const open = () => {
+    $modal.css('display', 'block');
+  };
+
+  // Modal hides
+  const close = () => {
+    $modal.css('display', 'none');
+  };
+
+  // Click events
+  $contactBtn.on('click', open);
+  $close.on('click', close);
 });
